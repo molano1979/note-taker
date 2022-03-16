60ac2bb85778ecb4959c12fd9bc3c4a8dc2aa733
Diana <dmmolano@yahoo.com>
2022-03-16 01:11:17 -0700

    init

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+  
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  
+  });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
